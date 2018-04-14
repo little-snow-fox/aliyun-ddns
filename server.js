de4ed56d2cfde6bbff275112f8de8df771d02dde
snowfox <snow@sfxh.cc>
2018-04-14 09:58:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // 这个服务端是部署在服务器的，目的是为了让客户端可以通过这个服务器获取IP而使用的。
-// 为什么不使用第三方服务器？别傻了，你20S请求人家一次，人家不封你IP？
+// 为什么不使用第三方服务器？你20S请求人家一次，人家不封你IP？
 app.get('/', function (req, res) {
   const strList = req.connection.remoteAddress.split(':');
   res.send({
